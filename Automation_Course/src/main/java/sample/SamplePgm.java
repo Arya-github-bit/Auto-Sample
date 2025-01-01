47947c4bb281fdde4afc8e226f94a5230d4000c4
Radhi <Radhi@192.168.1.6>
2025-01-01 22:42:08 +0530

    push

---
+++ Automation_Course/src/main/java/sample/SamplePgm.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class SamplePgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 9;
+		String b= "Arya";
+		System.out.println(a+" "+b);
+	}
+
+}
